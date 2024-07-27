fff64b148c6ea11be2eb6f08321381217e5683c0
Ege Sucu <ege.sucu@adesso.com.tr>
2024-07-27 22:58:48 +0300

    Initial Commit

---
+++ SwiftUI-UI-Test-Case/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-UI-Test-Case
+//
+//  Created by Sucu, Ege on 27.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-UI-Test-Case/SwiftUI_UI_Test_CaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UI_Test_CaseApp.swift
+//  SwiftUI-UI-Test-Case
+//
+//  Created by Sucu, Ege on 27.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UI_Test_CaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
